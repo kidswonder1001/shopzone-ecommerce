9b2d74713090db77658d1f535a153201251b9dbb
kidswonder1001 <kids.wonder1001@gmail.com>
2026-08-12 21:08:32 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error('MONGODB_URI is not defined in .env');
+    }
+    const conn = await mongoose.connect(uri);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
